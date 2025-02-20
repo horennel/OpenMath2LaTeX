13cb288035854c5c86744c874ed84c2cde1f4423
horennel <nelsonhoren@gmail.com>
2025-02-20 15:29:42 +0800

    perf: update main.py

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from threading import Thread
 
 import rumps
 
-from tools import onoff_q, open_settings, keyboard_q, get_config, MessageHelper, get_image, OpenAIHelper, to_clip, \
-    get_formula, start_key_listener, CFG_ERROR, ORC_ERROR, SUCCESS, NO_COPY_ERROR, WAIT
+from tools import create_new_listener, stop_listener, open_settings, keyboard_q, get_config, MessageHelper, get_image, \
+    OpenAIHelper, to_clip, get_formula, start_key_listener, CFG_ERROR, ORC_ERROR, SUCCESS, NO_COPY_ERROR, WAIT
 from backend import start_web
 
 
 class LatexOrcApplication(rumps.App):
     def __init__(self, name):
-        super(LatexOrcApplication, self).__init__(name=name, icon='./icons/menu_bar_off.png', quit_button="Quit")
+        super().__init__(name=name, icon='./icons/menu_bar_off.png', quit_button="Quit")
+        self.keyboard_listener = None
+        self.keyboard_stop_event = None
 
     @rumps.clicked("ORC on/off")
     def onoff(self, sender):
         if sender.state == 0:
-            onoff_q.put(True)
+            self._start_listener()
             self.icon = './icons/menu_bar_on.png'
             sender.state = 1
         else:
-            onoff_q.put(False)
+            self._stop_listener()
             self.icon = './icons/menu_bar_off.png'
             sender.state = 0
 
@@ -29,25 +31,36 @@ class LatexOrcApplication(rumps.App):
     def keyboard_q_monitoring(self):
         while True:
             keyboard_info = keyboard_q.get()
+            keyboard_q.empty()
             cfg = get_config()
             if not cfg:
                 rumps.notification(**MessageHelper(CFG_ERROR).to_json())
                 continue
             if keyboard_info['duration'] >= cfg.button_time and keyboard_info['keyboard'] == cfg.button_select:
-                image = get_image()
-                if image:
-                    rumps.notification(**MessageHelper(WAIT).to_json())
-                    err, result = OpenAIHelper(cfg.base_url, cfg.api_key, cfg.model).chat(image)
-                    if err:
-                        rumps.notification(**MessageHelper(ORC_ERROR, result).to_json())
-                    else:
-                        to_clip(get_formula(result))
-                        rumps.notification(**MessageHelper(SUCCESS).to_json())
-                else:
-                    rumps.notification(**MessageHelper(NO_COPY_ERROR).to_json())
+                self._process_image(cfg)
+
+    @staticmethod
+    def _process_image(cfg):
+        image = get_image()
+        if image:
+            rumps.notification(**MessageHelper(WAIT).to_json())
+            err, result = OpenAIHelper(cfg.base_url, cfg.api_key, cfg.model).chat(image)
+            if err:
+                rumps.notification(**MessageHelper(ORC_ERROR, result).to_json())
+            else:
+                to_clip(get_formula(result))
+                rumps.notification(**MessageHelper(SUCCESS).to_json())
+        else:
+            rumps.notification(**MessageHelper(NO_COPY_ERROR).to_json())
+
+    def _start_listener(self):
+        self.keyboard_listener, self.keyboard_stop_event = create_new_listener()
+        Thread(target=start_key_listener, args=(self.keyboard_listener, self.keyboard_stop_event)).start()
+
+    def _stop_listener(self):
+        stop_listener(self.keyboard_listener, self.keyboard_stop_event)
 
     def run(self, **options):
-        Thread(target=start_key_listener).start()
         Thread(target=self.keyboard_q_monitoring).start()
         Thread(target=start_web).start()
         super().run(**options)
