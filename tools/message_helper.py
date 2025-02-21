523883000a01b4dc7b2e438d6ce71ba9a19bb5ad
horennel <nelsonhoren@gmail.com>
2025-02-21 16:03:44 +0800

    style: update message_helper

--- tools/message_helper.py
+++ tools/message_helper.py
@@ -1,6 +1,6 @@
 SUCCESS = 0
 NO_COPY_ERROR = 1
-ORC_ERROR = 2
+OCR_ERROR = 2
 CFG_ERROR = 3
 WAIT = 4
 
@@ -11,7 +11,7 @@ class MessageHelper(object):
             SUCCESS: ("Success!", 'Success!Copied to clipboard.', ''),
             WAIT: ("Wait!", 'Image recognition in progress.', ''),
             NO_COPY_ERROR: ('Error!', 'You did not copy the screenshot.', 'Please copy the picture first.'),
-            ORC_ERROR: ('Error!', 'orc error', message),
+            OCR_ERROR: ('Error!', 'OCR error', message),
             CFG_ERROR: ('Error!', 'settings error', 'Please check the settings.')
         }
         self.title, self.subtitle, self.message = messages.get(msg_type, ('Error!', '', ''))
